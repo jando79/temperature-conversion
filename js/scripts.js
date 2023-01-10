542216b6f1f4d5c057bda0a4b7e5a6eb7fbdedaa
David Jandron <jandron1211@gmail.com>
2023-01-10 10:59:30 -0800

    Bugfix prompt not working or converting

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 //Business Logic
-function temperatureConversion() {
-  let = 
+function toCelsius() {
+  parseInt(faranheit -32 *5/9);
+  return
 }
 
 
@@ -10,6 +11,9 @@ function temperatureConversion() {
 
 
 
-
 //User Interface Logic
-const degrees = parseInt(prompt("Enter the temperature in Farenheit"));
+const faranheit = parseInt(prompt("Enter the temperature in Farenheit:"));
+//const toCelsius = parseInt(prompt("Degrees in Celsius:"))
+//The above created a prompt window to enter a number that is not a string
+
+window.alert(Celsius + "")
